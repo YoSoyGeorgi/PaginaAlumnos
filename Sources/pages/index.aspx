0494f2d9d072acf3191df8dddf5c649d5b3eb73a
Jorge Luis Olvera <holayosoygeorgi@outlook.com>
2022-04-19 01:27:45 -0500

    Cambios

---
+++ Sources/pages/index.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Sources/pages/webmaster.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="PaginaAlumnos.Sources.pages.index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+    Inicio
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- Sources/pages/webmaster.Master.designer.cs
+++ Sources/pages/webmaster.Master.designer.cs
@@ -33,13 +33,31 @@ namespace PaginaAlumnos.Sources.pages
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Control form1.
+        /// Control MP.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm MP;
+
+        /// <summary>
+        /// Control lblUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUser;
+
+        /// <summary>
+        /// Control usuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image usuario;
 
         /// <summary>
         /// Control ContentPlaceHolder1.
